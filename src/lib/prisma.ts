e9ac74f55dc1429958cf3239ea30af47955ef6ad
Ivandelucak <ivandelucakanina@gmail.com>
2026-05-06 14:22:43 -0300

    Configurar Prisma con variables de entorno

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASSWORD!,
-  database: "sistema_pagos",
+  host: process.env.DB_HOST ?? "localhost",
+  port: Number(process.env.DB_PORT ?? "3306"),
+  user: process.env.DB_USER ?? "root",
+  password: process.env.DB_PASSWORD ?? "",
+  database: process.env.DB_NAME ?? "sistema_pagos",
   allowPublicKeyRetrieval: true,
 });
 
